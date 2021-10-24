26d610d2acb4db6375fb066db43d1926e52b49f3
Medved-George <mathias.frere@yandex.ru>
2021-10-24 15:48:14 +0300

    fix: исправления в скрипте

--- scripts/script.js
+++ scripts/script.js
@@ -1,54 +1,35 @@
-let popUp = document.querySelector('.pop-up');
-let editButton = document.querySelector('.profile__edit-button');
-let closeButton = document.querySelector('.pop-up__close-button');
-let pageOverlay = document.querySelector('.page');
+let popUp = document.querySelector('.pop-up'); //Попап
+let editButton = document.querySelector('.profile__edit-button'); //Кнопка редактирования профиля
+let closeButton = document.querySelector('.pop-up__close-button'); //Кнопка закрытия попап
+
+let formElement = document.querySelector('.pop-up__form'); //Форму
+
+let nameInput = document.querySelector('.pop-up__input_type_name'); //Пле ввода имени
+let jobInput = document.querySelector('.pop-up__input_type_job'); //Поле ввода профессии
+
+let profileName = document.querySelector('.profile__name'); //Имя
+let profileJob = document.querySelector('.profile__job'); //Профессия
 
 function openPopUp () {
-    popUp.classList.add('pop-up_opened');
+    popUp.classList.add('pop-up_opened'); //Открытие попап
+    nameInput.value = profileName.textContent; //Записываем в поля редактирования данные из разметки
+    jobInput.value = profileJob.textContent;
 }
 
 function closePopUp () {
-    popUp.classList.remove('pop-up_opened');
+    popUp.classList.remove('pop-up_opened'); //Скрытие попап
 }
-
-let page = document.querySelector('.page');
-let overlay = document.querySelector('.overlay');
-
-function openPopUp () {
-    popUp.classList.add('pop-up_opened');
-    overlay.classList.add('overlay_active');
-}
-
-function closePopUp () {
-    popUp.classList.remove('pop-up_opened');
-    overlay.classList.remove('overlay_active');
-}
-
-editButton.addEventListener('click', openPopUp);
-closeButton.addEventListener('click', closePopUp);
-
-
-let formElement = document.querySelector('.pop-up__form');
-
-let nameInput = document.querySelector('.pop-up__input_type_name');
-let jobInput = document.querySelector('.pop-up__input_type_job');
-
-let profileName = document.querySelector('.profile__name');
-let profileJob = document.querySelector('.profile__job');
-
-nameInput.value = profileName.textContent;
-jobInput.value = profileJob.textContent;
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
 
-    let newProfileName = nameInput.value;
-    let newProfileJob = jobInput.value;
-
-    profileName.textContent = newProfileName;
-    profileJob.textContent = newProfileJob;
+    profileName.textContent = nameInput.value; //Записываем новые данные в разметку
+    profileJob.textContent = jobInput.value;
 
     closePopUp();
 }
+
+editButton.addEventListener('click', openPopUp);
+closeButton.addEventListener('click', closePopUp);
 
 formElement.addEventListener('submit', formSubmitHandler); 
